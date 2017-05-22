92e935f94534c59161ed7220389e389e5b747492
Kalu Kalu <1718221904@qq.com>
2017-05-23 00:06:05 +0800

    added migrations

--- frontpage/views.py
+++ frontpage/views.py
@@ -20,51 +20,57 @@ from . models import (
 def index(request):
     first_sections = FirstSection.objects.all()
     first_section = first_sections.first()
-    context = {'first_sections': first_sections, 'first_section': first_section}
-
-    # Get Second SubSection
-    context['second_subsection'] = SecondSection.objects.first()
-
-    # Get Third SubSection
-    context['third_subsection'] = ThirdSection.objects.first()
-
-    # Get Fourth SubSection
-    context['fourth_subsection'] = FourthSection.objects.first()
+    context = {}
 
 
-    # Get Fifth SubSection
-    context['fifth_section'] = FifthSection.objects.first()
+    try:
+        context = {'first_sections': first_sections, 'first_section': first_section}
+
+        # Get Second SubSection
+        context['second_subsection'] = SecondSection.objects.first()
+
+        # Get Third SubSection
+        context['third_subsection'] = ThirdSection.objects.first()
+
+        # Get Fourth SubSection
+        context['fourth_subsection'] = FourthSection.objects.first()
 
 
-    # Get Sixth SubSection
-    context['sixth_section'] = SixthSection.objects.first()
+        # Get Fifth SubSection
+        context['fifth_section'] = FifthSection.objects.first()
 
-    # Get Seventh SubSection
-    context['seventh_section'] = SeventhSection.objects.first()
 
-    # Get eighth SubSection
-    context['eighth_section'] = EighthSection.objects.first()
+        # Get Sixth SubSection
+        context['sixth_section'] = SixthSection.objects.first()
 
-    # Get eighth SubSection
-    context['eighth_section'] = EighthSection.objects.first()
+        # Get Seventh SubSection
+        context['seventh_section'] = SeventhSection.objects.first()
 
-    # Get ninth SubSection
-    context['ninth_section'] = NinthSection.objects.first()
+        # Get eighth SubSection
+        context['eighth_section'] = EighthSection.objects.first()
 
-    # Get tenth SubSection
-    context['tenth_section'] = TenthSection.objects.first()
+        # Get eighth SubSection
+        context['eighth_section'] = EighthSection.objects.first()
 
-    # Get Eleventh Section
-    context['eleventh_section'] = EleventhSection.objects.first()
+        # Get ninth SubSection
+        context['ninth_section'] = NinthSection.objects.first()
 
-    # Get Twelfth Section
-    context['twelfth_section'] = TwelfthSection.objects.first()
+        # Get tenth SubSection
+        context['tenth_section'] = TenthSection.objects.first()
 
-    # Get ThirteenthSection Section
-    context['thirteenth_section'] = ThirteenthSection.objects.first()
+        # Get Eleventh Section
+        context['eleventh_section'] = EleventhSection.objects.first()
 
-    # Get Footer Section
-    context['footer'] = Footer.objects.first()
+        # Get Twelfth Section
+        context['twelfth_section'] = TwelfthSection.objects.first()
+
+        # Get ThirteenthSection Section
+        context['thirteenth_section'] = ThirteenthSection.objects.first()
+
+        # Get Footer Section
+        context['footer'] = Footer.objects.first()
+    except:
+        pass
 
 
 
